e99159f0b62c6cf82a20bece1e82ceddbf0a1a61
Fyodor Sheremetyev <sheremetyev@gmail.com>
2012-11-05 20:46:31 +0000

    Don’t copy .lib file to project root directory.

--- build-debug.cmd
+++ build-debug.cmd
@@ -1,4 +1,3 @@
 @echo off
 call "%VS100COMNTOOLS%vsvars32.bat"
 devenv hunspell.sln /Build Debug
-xcopy /Y /D Debug\lib\*.lib .\
--- build-release.cmd
+++ build-release.cmd
@@ -1,4 +1,3 @@
 @echo off
 call "%VS100COMNTOOLS%vsvars32.bat"
 devenv hunspell.sln /Build Release
-xcopy /Y /D Release\lib\*.lib .\
